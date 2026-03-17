673781b342e7cd1b618984b7d7abf76744284db2
Emmanuel Pelletier <emmanuel.pelletier@mail.numerique.gouv.fr>
2026-03-17 18:35:27 +0100

    grid cells screen reader annoucement: prevent confusion on value/coords

--- app/client/components/GridView.ts
+++ app/client/components/GridView.ts
@@ -251,9 +251,10 @@ export default class GridView extends BaseView {
           if (field && rowId !== "new") {
             const value = this.tableModel.tableData.getValue(rowId as number, field.displayColModel().colId());
             const content = formatForScreenReader(field.formatter(), value);
+            // the row+column info is announced after a comma, to make the screen reader make a small pause
             this.gristDoc.appModel.screenReaderAnnouncer.announce(
               content,
-              `${t("row {{rowNum}} {{colName}}", { rowNum: cur.rowIndex + 1, colName: field.label() })}`,
+              `, ${t("row {{rowNum}} {{colName}}", { rowNum: cur.rowIndex + 1, colName: field.label() })}`,
             );
           }
         }
